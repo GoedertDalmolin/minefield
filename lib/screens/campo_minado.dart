765fadb7743ce7c292e928e5eaa596caaee1e05a
GoedertDalmolin <gbrunodalmolin@gmail.com>
2024-01-29 15:13:41 -0300

    Created campo_widget.dart.

--- lib/screens/campo_minado.dart
+++ lib/screens/campo_minado.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:minefield/components/campo_widget.dart';
 import 'package:minefield/components/resultado_widget.dart';
+import 'package:minefield/models/campo.dart';
+import 'package:minefield/models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({super.key});
 
+  _alternarMarcacao(Campo campo) {}
+
+  _abrir(Campo campo) {}
+
   @override
   Widget build(BuildContext context) {
+    Campo vizinho1 = Campo(linha: 1, coluna: 0);
+    vizinho1.minar();
+
+    Campo vizinho2 = Campo(linha: 1, coluna: 1);
+    vizinho2.minar();
+
+    Campo campo = Campo(linha: 0, coluna: 0);
+    campo.adicionarVizinho(vizinho1);
+    campo.adicionarVizinho(vizinho2);
+
+    try {
+      // campo.minar();
+      // campo.alternarMarcacao();
+      campo.abrir();
+    } on ExplosaoException {}
+
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
-          venceu:  true,
+          venceu: true,
           onReiniciar: () {},
         ),
         body: Container(
-          child: Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );
